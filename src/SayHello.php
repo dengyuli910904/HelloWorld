9b5a35ef5f2d431b3f57bd3df010b2488d14357b
lily <912617726@qq.com>
2017-10-19 14:11:27 +0800

    my first composer demo to echo hello world

---
+++ src/SayHello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lily
+ * Date: 2017/10/19
+ * Time: 11:40
+ */
+namespace Hello;
+
+class SayHello{
+    public static function world(){
+        return 'Hello World';
+    }
+}
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lily
+ * Date: 2017/10/19
+ * Time: 11:46
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+use Hello\SayHello;
+echo SayHello::world();
